c1d62e8826ff47ff3ee253eac6b15e03bf4c13b5
Allen Rothschild <9AR9@users.noreply.github.com>
2021-10-28 09:53:42 -0700

    Bottom separator added back in

--- src/components/LabelBackstop.js
+++ src/components/LabelBackstop.js
@@ -294,6 +294,7 @@ export default class LabelBackstop extends React.Component {
                             approvalsSubheader={this.state.approvalsSubheader}
                             handleUserInput={this.handleUserInput}
                         />
+                        <JuxceSeparator />
                     </Col>
                 </Row>
             </Container>
